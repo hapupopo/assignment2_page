e8eb4a7b79d9364961b9dbff726668b5990141d4
MH Augustine Kim <63154221+hapupopo@users.noreply.github.com>
2022-06-27 13:18:20 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-server = app.server
 
 @app.route("/")
 def home():
@@ -11,5 +10,5 @@ def home():
 def detail():
     return render_template('portfolio_details.html')
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=5000, debug=True)
